08c8e809df2458a3cc66963457222526e00cefd9
realeed <makonygina@yandex.ru>
2024-04-14 16:50:17 +0300

    комментарии

--- data/types.py
+++ data/types.py
@@ -1,2 +1,3 @@
 class QuestionType():
-    types_list = {0: 'python', 1: 'c++'}
+    types_list = {0: 'python', 1: 'c++', 2: 'программирование', 3: "web",
+                  4: "data base", 5: "html", 6: "другое"}
